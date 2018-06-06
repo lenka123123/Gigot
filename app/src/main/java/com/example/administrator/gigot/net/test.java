15869273b7f0d20fbdd84893b49a51e0176fb1dd
lenka123123 <13902276741@163.com>
2018-06-06 22:49:11 +0800

    测试一下

--- app/src/main/java/com/example/administrator/gigot/net/test.java
+++ app/src/main/java/com/example/administrator/gigot/net/test.java
@@ -1,4 +1,12 @@
 package com.example.administrator.gigot.net;
 
-public class test {
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class test extends Activity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
